b09ebb0766e72008319bc8315f568862d79a04a4
B6X35 <d.b.palamarchuk@gmail.com>
2021-11-17 11:19:14 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,10 +6,12 @@ const startBtn = document.querySelector("button[data-start]");
 const stopBtn = document.querySelector("button[data-stop]");
 const body = document.querySelector('body');
 let timerId = null;
+stopBtn.disabled = true;
 
 startBtn.addEventListener("click", event => {
     body.style.backgroundColor = getRandomHexColor();
     startBtn.disabled = true;
+    stopBtn.disabled = false;
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
@@ -19,4 +21,5 @@ startBtn.addEventListener("click", event => {
 stopBtn.addEventListener("click", () => {
   clearInterval(timerId);
   startBtn.disabled = false;
+  stopBtn.disabled = true;
 });
